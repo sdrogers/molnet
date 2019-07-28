877f010738e8d61e9ce4622750f90c67ba73352e
Madeleine Ernst <enmeilin@gmail.com>
2019-07-28 13:33:01 +0200

    added line for filtered peak area header

--- code/mnet_utilities.py
+++ code/mnet_utilities.py
@@ -206,7 +206,10 @@ def load_peak_areas(mzmine_ms1_file):
         reader = csv.reader(f)
         heads = next(reader)
         # dictionary of filenames and their columns
-        file_name_cols = {h.split('Peak area')[0].rstrip():i for i,h in enumerate(heads) if 'Peak area' in h}
+        if 'filtered' in heads[3]:
+            file_name_cols = {h.split('filtered Peak area')[0].rstrip():i for i,h in enumerate(heads) if 'filtered Peak area' in h}
+        else: 
+            file_name_cols = {h.split('Peak area')[0].rstrip():i for i,h in enumerate(heads) if 'Peak area' in h}
         peak_areas = {}
         for line in reader:
             peak_id = int(line[0])
